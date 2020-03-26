1c0b35dd592e3fe9e9b895146c885d3e2eb80d4b
Mark <mcampmier@gmail.com>
2020-03-26 14:28:54 -0500

    update field assignment

--- PurpleRainLS.py
+++ PurpleRainLS.py
@@ -259,6 +259,10 @@ def build_hdf(name_list, hdfname, tzstr, date_ind, lat, lon):
             else:
                 no_b = False
             sensors.append(name_list[i][0].replace('Primary_', '').split('_20')[0])
+            print(pa)
+            print(sa)
+            print(pb)
+            print(sb)
             pa = pd.read_csv(pa, skip_blank_lines=False)
             sa = pd.read_csv(sa, skip_blank_lines=False)
             pb = pd.read_csv(pb, skip_blank_lines=False)
